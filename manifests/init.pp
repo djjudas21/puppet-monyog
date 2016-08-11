8330bbd0f6df48b813f6b5dd12b66dcbb28d3dd6
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-08-11 15:42:24 +0100

    Basic manifest that allows the user to set the port

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,35 @@
 #
 # Copyright 2016 Your name here, unless otherwise noted.
 #
-class monyog {
+class monyog (
+  $basedir = '/usr/local/MONyog',
+  $inifile = "${basedir}/MONyog.ini",
+  $port    = 5555,
+) {
 
+  package { 'MONyog':
+    ensure => installed,
+  }
+
+  ini_setting { 'Port':
+    path    => $inifile,
+    section => 'GENERAL',
+    setting => 'Port'
+    value   => $port,
+    require => Package['MONyog'],
+    notify  => Service['MONyogd'],
+  }
+
+  firewall { '100-monyog':
+    proto  => 'tcp',
+    dport  => $port,
+    action => 'accept',
+  }
+
+  service { 'MONyogd':
+    ensure  => running,
+    enable  => true,
+    require => Package['MONyog'],
+  }
 
 }
